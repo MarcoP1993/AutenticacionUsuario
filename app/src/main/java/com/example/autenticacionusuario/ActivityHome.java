df15ac97db473278eda0f01bee34c6c880fc4d11
Marco Antonio <perea_10@hotmail.com>
2020-11-24 20:36:27 +0100

    Usuario y contraseña v1.2 actualizado

--- app/src/main/java/com/example/autenticacionusuario/ActivityHome.java
+++ app/src/main/java/com/example/autenticacionusuario/ActivityHome.java
@@ -6,19 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import Clases.Usuario;
+
 public class ActivityHome extends AppCompatActivity {
 
     TextView txt_saludo = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         txt_saludo = (TextView) findViewById(R.id.txt_saludo);
         Intent intent = getIntent();
-        if(intent !=null) {
+        if (intent != null) {
             String mensaje = intent.getStringExtra(MainActivity.EXTRAEMAIL);
-            txt_saludo.setText("BIENVENIDO " + mensaje);
-        }
+            if (mensaje != null) {
+                txt_saludo.setText("BIENVENIDO" + mensaje);
+            } else {
+                Usuario user1 = (Usuario) intent.getSerializableExtra(PantallaRegistro.EXTRA_OBJETO_USUARIO);
+                if (user1 != null) {
+                    txt_saludo.setText("BIENVENIDO NUEVO USUARIO" + user1.getNombre());
+                }
+            }
 
+        }
     }
 }
--- app/src/main/java/com/example/autenticacionusuario/PantallaRegistro.java
+++ app/src/main/java/com/example/autenticacionusuario/PantallaRegistro.java
@@ -2,13 +2,51 @@ package com.example.autenticacionusuario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import Clases.Usuario;
 
 public class PantallaRegistro extends AppCompatActivity {
+    public static final String EXTRA_OBJETO_USUARIO = "com.example.autenticacionusuario.objetousuario";
+    EditText edt_nombre = null;
+    EditText edt_email_registro = null;
+    EditText edt_password_registro = null;
+    EditText edt_password_confirmacion = null;
+    EditText edt_telefono = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_registro);
+        edt_nombre = findViewById(R.id.edt_nombre);
+        edt_email_registro = findViewById(R.id.edt_email);
+        edt_password_registro = findViewById(R.id.edt_passwordregistro);
+        edt_password_confirmacion = findViewById(R.id.edt_passwordrepetir);
+        edt_telefono = findViewById(R.id.edt_telefono);
+    }
+
+    public void registrarse(View view) {
+        String password1 = String.valueOf(edt_password_registro.getText());
+        String password2 = String.valueOf(edt_password_confirmacion.getText());
+        if (!password1.equals(password2)) {
+            edt_password_registro.setError("Las contraseñas no coinciden");
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String nombre = String.valueOf(edt_nombre.getText());
+        String emailRegistro = String.valueOf(edt_email_registro.getText());
+        String telefono = String.valueOf(edt_telefono);
+        Usuario user1 = new Usuario(nombre, emailRegistro, password1, telefono);
+        Intent intent = new Intent(this, ActivityHome.class);
+        intent.putExtra(EXTRA_OBJETO_USUARIO, user1);
+        startActivity(intent);
+    }
+
+    public void volverInicio(View view) {
+        finish();
     }
 }
